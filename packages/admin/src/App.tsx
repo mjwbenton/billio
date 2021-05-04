d71ab792011e2a18c032d73a3c7eb36c22fbd980
Matthew Benton <m@mattb.tech>
2021-05-04 14:50:36 +0100

    Some simple themeing

--- packages/admin/src/App.tsx
+++ packages/admin/src/App.tsx
@@ -1,4 +1,4 @@
-import { Admin, Resource, ListGuesser, EditGuesser } from "react-admin";
+import { Admin, Resource } from "react-admin";
 import dataProvider from "./dataProvider";
 import authProvider from "./authProvider";
 import GamesIcon from "@material-ui/icons/Games";
@@ -7,10 +7,44 @@ import { BookCreate, BookEdit } from "./book/BookEdit";
 import { VideoGameCreate, VideoGameEdit } from "./videogame/VideoGameEdit";
 import { VideoGameShow, VideoGameList } from "./videogame/VideoGameShow";
 import { BookShow, BookList } from "./book/BookShow";
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const THEME = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#7c3aed",
+    },
+    secondary: {
+      main: "#10b981",
+      contrastText: "#fafafa",
+    },
+    grey: {},
+  },
+  overrides: {
+    MuiFilledInput: {
+      root: {
+        backgroundColor: "rgba(0,0,0,0.05)",
+      },
+    },
+  },
+  typography: {
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Arial",
+      "sans-serif",
+    ].join(","),
+  },
+});
 
 function App() {
   return (
-    <Admin dataProvider={dataProvider} authProvider={authProvider}>
+    <Admin
+      theme={THEME}
+      dataProvider={dataProvider}
+      authProvider={authProvider}
+    >
       <Resource
         name="VideoGame"
         create={VideoGameCreate}
