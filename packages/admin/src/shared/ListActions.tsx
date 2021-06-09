d4eb080820309f70eba57e54d50aa059621ae906
Matthew Benton <m@mattb.tech>
2021-06-09 12:02:00 +0100

    Improve ListActions layout using mui grid

--- packages/admin/src/shared/ListActions.tsx
+++ packages/admin/src/shared/ListActions.tsx
@@ -1,3 +1,4 @@
+import { Grid } from "@material-ui/core";
 import { CreateButton, sanitizeListRestProps, TopToolbar } from "react-admin";
 import ImportButton from "../import/ImportButton";
 
@@ -5,8 +6,14 @@ export default function ListActions(props: any) {
   const { className, exporter, filters, maxResults, ...rest } = props;
   return (
     <TopToolbar className={className} {...sanitizeListRestProps(rest)}>
-      <CreateButton variant="text" color="secondary" />
-      <ImportButton variant="text" color="primary" />
+      <Grid container direction="row" spacing={2}>
+        <Grid item xs={6}>
+          <CreateButton variant="text" color="secondary" />
+        </Grid>
+        <Grid item xs={6}>
+          <ImportButton variant="text" color="primary" />
+        </Grid>
+      </Grid>
     </TopToolbar>
   );
 }
