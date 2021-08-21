90ed76eff1b7a933fcbea409e75a36a64679e0ed
Matthew Benton <m@mattb.tech>
2021-08-21 20:53:26 +0100

    Use prod table for export

--- packages/bulk-export/src/bin/export.ts
+++ packages/bulk-export/src/bin/export.ts
@@ -1,7 +1,7 @@
 process.env.AWS_PROFILE = "mattb.tech-deploy";
 process.env.AWS_REGION = "us-east-1";
-// process.env.BILLIO_TABLE = "BillioData-ItemTable276B2AC8-1HIYN64N2BKA1";
-process.env.BILLIO_TABLE = "BillioTestData-ItemTable276B2AC8-1L7WY9SA7KB61";
+process.env.BILLIO_TABLE = "BillioData-ItemTable276B2AC8-1HIYN64N2BKA1";
+//process.env.BILLIO_TABLE = "BillioTestData-ItemTable276B2AC8-1L7WY9SA7KB61";
 
 import { fetchAllForType } from "../index";
 import fs from "fs/promises";
