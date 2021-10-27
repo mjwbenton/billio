42f9b2e268d09e5a189e09b484ff66bcbb9c540f
Matthew Benton <m@mattb.tech>
2021-10-27 16:35:25 +0100

    Support more complex namespacedIds

--- packages/graphql/src/shared/parseNamespacedId.ts
+++ packages/graphql/src/shared/parseNamespacedId.ts
@@ -1,31 +1,43 @@
 export default function parseNamespacedId(
   namespacedId: string,
-  options?: { assertNamespace?: string }
+  {
+    assertNamespace,
+    idSections = 2,
+  }: { assertNamespace?: string; idSections?: number } = {}
 ) {
-  const [namespace, innerId, more] = namespacedId.split(":");
+  const [namespace, innerId, ...more] = namespacedId.split(":");
 
-  if (more || !innerId) {
+  if (!innerId) {
     throw new Error(`Invalid id structure: "${namespacedId}"`);
   }
 
-  if (options?.assertNamespace && namespace !== options?.assertNamespace) {
+  if (assertNamespace && namespace !== assertNamespace) {
     throw new Error(
-      `Incorrect namespace on id "${namespacedId}", expecting ${options?.assertNamespace}`
+      `Incorrect namespace on id "${namespacedId}", expecting ${assertNamespace}`
+    );
+  }
+
+  if (more.length != idSections - 2) {
+    throw new Error(
+      `Invalid number of sections ${more.length + 2}, expecting ${idSections}`
     );
   }
 
   return {
     namespace,
     externalId: innerId,
+    additionalSections: more,
   };
 }
 
 export function buildNamespacedId({
   namespace,
   externalId,
+  additionalSections = [],
 }: {
   namespace: string;
   externalId: string;
+  additionalSections?: Array<string>;
 }) {
-  return `${namespace}:${externalId}`;
+  return [namespace, externalId, ...additionalSections].join(":");
 }
