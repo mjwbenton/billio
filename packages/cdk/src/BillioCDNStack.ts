bc00814f803549e22b4ddddd28cb7514675963c3
Matthew Benton <m@mattb.tech>
2024-08-14 16:47:21 +0100

    Set Cache-control header for images coming from cloudfront

--- packages/cdk/src/BillioCDNStack.ts
+++ packages/cdk/src/BillioCDNStack.ts
@@ -1,6 +1,10 @@
 import { Stack } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { Distribution, ViewerProtocolPolicy } from "aws-cdk-lib/aws-cloudfront";
+import {
+  Distribution,
+  ResponseHeadersPolicy,
+  ViewerProtocolPolicy,
+} from "aws-cdk-lib/aws-cloudfront";
 import { S3Origin } from "aws-cdk-lib/aws-cloudfront-origins";
 import { CloudFrontTarget } from "aws-cdk-lib/aws-route53-targets";
 import DomainConstruct from "./DomainConstruct";
@@ -30,6 +34,21 @@ export default class BillioCDNStack extends Stack {
       defaultBehavior: {
         origin: new S3Origin(imageStack.imageBucket),
         viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        responseHeadersPolicy: new ResponseHeadersPolicy(
+          this,
+          "ResponseHeadersPolicy",
+          {
+            customHeadersBehavior: {
+              customHeaders: [
+                {
+                  header: "Cache-Control",
+                  value: "max-age=31536000",
+                  override: true,
+                },
+              ],
+            },
+          },
+        ),
       },
       domainNames: [domain.name],
       certificate: domain.certificate,
