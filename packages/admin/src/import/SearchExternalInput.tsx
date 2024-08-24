ae8f380a06e6cfe1690c3fb0daf3734616b523f0
Matthew Benton <m@mattb.tech>
2024-08-24 12:13:32 +0100

    Add some bottom margin to the search input

--- packages/admin/src/import/SearchExternalInput.tsx
+++ packages/admin/src/import/SearchExternalInput.tsx
@@ -1,6 +1,7 @@
 import { useInput } from "react-admin";
 import Autocomplete from "../shared/Autocomplete";
 import useSearchExternal from "../shared/useSearchExternal";
+import { Box } from "@material-ui/core";
 
 export default function SearchExternalInput({ source }: { source: string }) {
   const { options, setSearchTerm } = useSearchExternal();
@@ -11,18 +12,20 @@ export default function SearchExternalInput({ source }: { source: string }) {
   } = useInput({ source });
 
   return (
-    <Autocomplete
-      options={options}
-      filterByTitle={false}
-      onSelection={(option) => {
-        onChange(option?.id);
-      }}
-      onInputChange={(value) => {
-        setSearchTerm(value);
-      }}
-      label="search"
-      name={name}
-      error={!!(touched && error)}
-    />
+    <Box sx={{ mb: 3 }}>
+      <Autocomplete
+        options={options}
+        filterByTitle={false}
+        onSelection={(option) => {
+          onChange(option?.id);
+        }}
+        onInputChange={(value) => {
+          setSearchTerm(value);
+        }}
+        label="search"
+        name={name}
+        error={!!(touched && error)}
+      />
+    </Box>
   );
 }
