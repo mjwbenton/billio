b854108d346729c0fc925d4646696088bb66cce0
Matthew Benton <m@mattb.tech>
2025-05-28 16:53:32 +0100

    Update local references to new tables

--- packages/graphql/src/devServer.ts
+++ packages/graphql/src/devServer.ts
@@ -5,17 +5,17 @@ process.env.AWS_REGION = "us-east-1";
 
 const STACKS = {
   local: {
-    BILLIO_TABLE: "BillioLocalDataV2-ItemTable276B2AC8-ZR6ILIEIHBZQ",
+    BILLIO_TABLE: "BillioLocalDataV3-ItemTable276B2AC8-1TR74TBIIY3RR",
     BILLIO_IMAGE_BUCKET: "billiotestimage-imagebucket97210811-jdlm9v72wtlf",
     BILLIO_IMAGE_DOMAIN: "https://image-cdn-test.billio.mattb.tech",
   },
   test: {
-    BILLIO_TABLE: "BillioTestDataV2-ItemTable276B2AC8-1SP3QGNGZSODK",
+    BILLIO_TABLE: "BillioTestDataV3-ItemTable276B2AC8-2I5WC5N7N5R0",
     BILLIO_IMAGE_BUCKET: "billiotestimage-imagebucket97210811-jdlm9v72wtlf",
     BILLIO_IMAGE_DOMAIN: "https://image-cdn-test.billio.mattb.tech",
   },
   prod: {
-    BILLIO_TABLE: "BillioDataV2-ItemTable276B2AC8-1NBN3P38AYOHE",
+    BILLIO_TABLE: "BillioDataV3-ItemTable276B2AC8-71REK85N61EZ",
     BILLIO_IMAGE_BUCKET: "billioimage-imagebucket97210811-1aeflaj405d4g",
     BILLIO_IMAGE_DOMAIN: "https://image-cdn.billio.mattb.tech",
   },
