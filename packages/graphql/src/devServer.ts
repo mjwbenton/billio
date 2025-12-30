0f45c63f65ac748f356fc3c6fb5fa1375aaaa414
Matthew Benton <m@mattb.tech>
2025-12-30 21:17:01 +0000

    Fix dev server

--- packages/graphql/src/devServer.ts
+++ packages/graphql/src/devServer.ts
@@ -1,21 +1,21 @@
 require("dotenv").config();
 process.env.AWS_SDK_LOAD_CONFIG = "1";
-process.env.AWS_PROFILE = "admin-legacy-sso";
+process.env.AWS_PROFILE = "admin";
 process.env.AWS_REGION = "us-east-1";
 
 const STACKS = {
   local: {
-    BILLIO_TABLE: "BillioLocalDataV3-ItemTable276B2AC8-1TR74TBIIY3RR",
+    BILLIO_DSQL_ENDPOINT: "3btnvhteqsynk2pmi2iikzsxsm.dsql.us-east-1.on.aws",
     BILLIO_IMAGE_BUCKET: "billiotestimage-imagebucket97210811-jdlm9v72wtlf",
     BILLIO_IMAGE_DOMAIN: "https://image-cdn-test.billio.mattb.tech",
   },
   test: {
-    BILLIO_TABLE: "BillioTestDataV3-ItemTable276B2AC8-2I5WC5N7N5R0",
+    BILLIO_DSQL_ENDPOINT: "cztnvhszomywukvrfxko4fhjrq.dsql.us-east-1.on.aws",
     BILLIO_IMAGE_BUCKET: "billiotestimage-imagebucket97210811-jdlm9v72wtlf",
     BILLIO_IMAGE_DOMAIN: "https://image-cdn-test.billio.mattb.tech",
   },
   prod: {
-    BILLIO_TABLE: "BillioDataV3-ItemTable276B2AC8-71REK85N61EZ",
+    BILLIO_DSQL_ENDPOINT: "p5tnvhwkusq4zbisuilfpmek34.dsql.us-east-1.on.aws",
     BILLIO_IMAGE_BUCKET: "billioimage-imagebucket97210811-1aeflaj405d4g",
     BILLIO_IMAGE_DOMAIN: "https://image-cdn.billio.mattb.tech",
   },
@@ -23,7 +23,7 @@ const STACKS = {
 const stack: keyof typeof STACKS =
   (process.env.INFRA_STACK as keyof typeof STACKS) ?? "local";
 
-process.env.BILLIO_TABLE = STACKS[stack].BILLIO_TABLE;
+process.env.BILLIO_DSQL_ENDPOINT = STACKS[stack].BILLIO_DSQL_ENDPOINT;
 process.env.BILLIO_IMAGE_BUCKET = STACKS[stack].BILLIO_IMAGE_BUCKET;
 process.env.BILLIO_IMAGE_DOMAIN = STACKS[stack].BILLIO_IMAGE_DOMAIN;
 process.env.ENABLE_MUTATIONS = "1";
